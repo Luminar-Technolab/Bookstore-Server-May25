09c8e302f3b2cb7febfb1a2f900bce6505b492bf
soniagkalliath <soniageorge111@gmail.com>
2025-10-10 11:06:51 +0530

    server initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//import dotenv express cors
+// Loads .env file contents into process.env
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const router = require('./routing/router')
+
+//create server
+const bookstoreServer = express()
+//enable cors protocol in server app
+bookstoreServer.use(cors())
+bookstoreServer.use(express.json()) // parse json 
+bookstoreServer.use(router)
+//create port
+const PORT = 3000 
+
+//RUN SERVER PORT
+bookstoreServer.listen(PORT,()=>{
+    console.log(`BookStore Server started at PORT: ${PORT}, and waiting for client request!!!`);
+})
+
+//resolving http request
+bookstoreServer.get('/',(req,res)=>{
+    res.status(200).send(`<h1 style="color:blue;">BookStore Server started...and waiting for client requests!!! </h1>`)
+})
