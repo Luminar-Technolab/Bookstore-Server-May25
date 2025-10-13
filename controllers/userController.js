049494832cccc34e8af19e9e35e50a32b6612451
soniagkalliath <soniageorge111@gmail.com>
2025-10-13 11:27:09 +0530

    register and login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ exports.registerController = async (req,res)=>{
     console.log("Inside Register API");
     // console.log(req.body);
     const {username,email,password} = req.body
-    console.log(username,email,password);
+    // console.log(username,email,password);
     try{
         const existingUser = await users.findOne({email})
         if(existingUser){
@@ -24,4 +24,20 @@ exports.registerController = async (req,res)=>{
     }
 }
 //login
+exports.loginController = async (req,res)=>{
+    console.log("Inside Login API");
+    // console.log(req.body);
+    const {email,password} = req.body
+    console.log(email,password);
+    try{
+        const existingUser = await users.findOne({email,password})
+        if(existingUser){
+            res.status(200).json({user:existingUser})
+        }else{
+           res.status(404).json("Invalid Email / Password")
+        }
+    }catch(err){
+        res.status(500).json(err)
+    }
+}
 //profile
--- routing/router.js
+++ routing/router.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 //register
 router.post('/register',userController.registerController)
-
+//login
+router.post('/login',userController.loginController)
 
 module.exports = router
