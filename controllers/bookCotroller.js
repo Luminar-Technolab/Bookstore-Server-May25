2f630252c611f2d9b298ec34a39022cd3db58f00
soniagkalliath <soniageorge111@gmail.com>
2025-10-16 11:03:47 +0530

    jwt middleware added

---
+++ controllers/bookCotroller.js
@@ -0,0 +1,7 @@
+const books = require('../models/bookModel')
+
+//add book
+exports.addBookController =  (req,res)=>{
+    console.log("Inside addBookController");
+    res.status(200).json("request recieved!!!")
+}
--- routing/router.js
+++ routing/router.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const bookController = require('../controllers/bookCotroller')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
 
 const router = express.Router()
 
@@ -9,5 +11,7 @@ router.post('/register',userController.registerController)
 router.post('/login',userController.loginController)
 //google login
 router.post('/google-login',userController.googleLoginController)
+//add-book
+router.post('/add-book',jwtMiddleware,bookController.addBookController)
 
 module.exports = router
