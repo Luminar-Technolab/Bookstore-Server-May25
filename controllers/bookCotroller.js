14631aa697bb660855a0c7033b9e3ed603e0be21
soniagkalliath <soniageorge111@gmail.com>
2025-10-17 12:36:13 +0530

    add book controller completed

--- controllers/bookCotroller.js
+++
@@ -1,7 +0,0 @@
-const books = require('../models/bookModel')
-
-//add book
-exports.addBookController =  (req,res)=>{
-    console.log("Inside addBookController");
-    res.status(200).json("request recieved!!!")
-}
